5d8ebfdabb6c9c686618647ede7b55190fa35c99
Ben Siron <ben@siron.net>
2015-06-29 21:45:46 -0500

    disabling of warnings with annotations

--- source/secureStringWindow.xaml.cs
+++ source/secureStringWindow.xaml.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Security.Cryptography;
 using System.Windows;
@@ -8,18 +9,18 @@ namespace UGTS.Encoder
 {
 	public partial class SecureStringWindow
 	{
-	    public Observable<string> Username { get; set; }
-	    public Observable<string> Password { get; set; }
-	    public Observable<string> Plaintext { get; set; }
-	    public Observable<string> Ciphertext { get; set; }
-        public Observable<string> Setting { get; set; }
-        public Observable<bool> SystemAccount { get; set; }
-        public Observable<bool> ShowPassword { get; set; }
-        public Computed<bool> IsPasswordEnabled { get; set; }
-        public Computed<Visibility> IsPasswordVisible { get; set; }
-        public Computed<Visibility> IsPasswordTextVisible { get; set; }
-        public Computed<bool> IsEncodeEnabled { get; set; }
-        public Computed<bool> IsDecodeEnabled { get; set; }
+        [UsedImplicitly] public Observable<string> Username { get; set; }
+	    [UsedImplicitly] public Observable<string> Password { get; set; }
+	    [UsedImplicitly] public Observable<string> Plaintext { get; set; }
+        [UsedImplicitly] public Observable<string> Ciphertext { get; set; }
+        [UsedImplicitly] public Observable<string> Setting { get; set; }
+        [UsedImplicitly] public Observable<bool> SystemAccount { get; set; }
+        [UsedImplicitly] public Observable<bool> ShowPassword { get; set; }
+        [UsedImplicitly] public Computed<bool> IsPasswordEnabled { get; set; }
+        [UsedImplicitly] public Computed<Visibility> IsPasswordVisible { get; set; }
+        [UsedImplicitly] public Computed<Visibility> IsPasswordTextVisible { get; set; }
+        [UsedImplicitly] public Computed<bool> IsEncodeEnabled { get; set; }
+        [UsedImplicitly] public Computed<bool> IsDecodeEnabled { get; set; }
 
         public SecureStringWindow() 
 		{
