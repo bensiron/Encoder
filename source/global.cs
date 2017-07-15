2109244d91b77213e037e50f5ae555e6e36d5182
Ben Siron <ben@siron.net>
2017-07-14 19:45:38 -0500

    log4net config

--- source/global.cs
+++ source/global.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Windows;
+using log4net;
+using log4net.Config;
 using UGTS.UI;
 
 namespace UGTS.Encoder
 {
-	public static class MGlobal
+    public static class MGlobal
 	{
+	    private static readonly ILog log = LogManager.GetLogger(typeof (MGlobal));
+
         [STAThread]
 		public static void Main()
-		{
-            MUI.Run(Run, "Encoder", "encoder.ico");
-		}
+        {
+            XmlConfigurator.Configure();
+            log.Info("run begin");
 
-	    private static void Run()
+            MUI.Run(Run, "Encoder", "encoder.ico");
+
+            log.Info("run end");
+        }
+
+        private static void Run()
         {
 	        var w = new SecureStringWindow {WindowStartupLocation = WindowStartupLocation.CenterScreen};
 	        w.ShowDialog();
