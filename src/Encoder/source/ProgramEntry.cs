06e167ff16469b89b092138d95845e7ca8374151
Ben Siron <ben@siron.net>
2019-03-12 19:42:20 -0500

    remove log4net dependencies

--- src/Encoder/source/ProgramEntry.cs
+++ src/Encoder/source/ProgramEntry.cs
@@ -1,28 +1,18 @@
 using System;
 using System.Windows;
-using log4net;
-using log4net.Config;
 
 namespace UGTS.Encoder
 {
     public static class ProgramEntry
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof (ProgramEntry));
-
         [STAThread]
         public static void Main()
         {
             try
             {
-                XmlConfigurator.Configure();
-                Log.Info("run begin");
-
                 WPF.MMain.IconPath = "encoder.ico";
-
                 var w = new SecureStringWindow { WindowStartupLocation = WindowStartupLocation.CenterScreen };
                 w.ShowDialog();
-
-                Log.Info("run end");
             }
             catch (Exception ex)
             {
